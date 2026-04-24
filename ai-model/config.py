128de9bca170467772b59c9cc30cfe7a76d2d2c4
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-04-25 03:10:59 +0500

    config.py file is edited. directory of testing & training images and labels saved

---
+++ ai-model/config.py
@@ -0,0 +1,9 @@
+# dataset folder locations saving process
+DIR = "./data/mnist/"
+
+
+TRAINING_IMAGE = DIR + "train-images.idx3-ubyte" # training images diretory 
+TESTING_IMAGE = DIR + "t10k-images.idx3-ubyte" # testing images diretory 
+
+TRAINING_LABELS = DIR + "train-labels.idx1-ubyte" # training labels directory
+TESTING_LABELS = DIR + "t10k-labels.idx1-ubyte" # testing labels directory
