6b15bdd48ee5aa884a46c8525e8255088d237423
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-04-25 16:12:51 +0500

    I added normalization settings statics variables to config.py file.

--- ai-model/config.py
+++ ai-model/config.py
@@ -7,3 +7,7 @@ TESTING_IMAGE = DIR + "t10k-images.idx3-ubyte" # testing images diretory
 
 TRAINING_LABELS = DIR + "train-labels.idx1-ubyte" # training labels directory
 TESTING_LABELS = DIR + "t10k-labels.idx1-ubyte" # testing labels directory
+
+# image normalization settings
+MAX_PIXEL = 255.0
+NUMBER_OF_CLASSES = 10
